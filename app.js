feda6105f3506b61eaa59a4571e1eea8a2ac41c8
jashmehta3300 <jashmehta3300@gmail.com>
2020-05-21 21:07:02 +0530

    basic graphql setup done

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
+const {buildSchema} = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/graphql',graphqlHttp({
-	schema: null,
-	rootValue: {}
+	schema: buildSchema(`
+		type rootQuery {
+			events: [String!]!
+		}
+		type rootMutation {
+			createEvent(name: String): String
+		}
+		schema {
+			query: rootQuery,
+			mutation: rootMutation
+		}
+	`),
+	//bundle of all resolvers
+	rootValue: {
+		//controller functions(same name as written in type)
+		events: () => {
+			return ['Cooking','Sailing','coding'];
+		},
+		createEvent: (args) => {
+			const eventName = args.name;
+			return eventName;
+		}
+	}
 }
 );
 
