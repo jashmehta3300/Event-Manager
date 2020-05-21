361d6567ee5d554dd521494cf03816f19f97d434
jashmehta3300 <jashmehta3300@gmail.com>
2020-05-21 20:35:59 +0530

    graphql added

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/',(req,res,next) => {
-	res.send('Hello World!');
-})
+app.use('/graphql',graphqlHttp({
+	schema: null,
+	rootValue: {}
+}
+);
 
 app.listen(3000);
 
