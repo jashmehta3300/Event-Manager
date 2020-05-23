1c2bdfa80d9d64faa9708e6140f0199de87f7d6c
jashmehta3300 <jashmehta3300@gmail.com>
2020-05-23 15:58:38 +0530

    event schema added

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors')
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const connectDB = require('./config/db');
+const Event = require('./models/event')
+
+//load env vars
+dotenv.config({
+  path: './config/config.env'
+});
+
+//Connect to database
+connectDB();
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
 app.use(
-	'/graphql',
-	graphqlHttp({
-		schema: buildSchema(`
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
         type Event {
           _id: ID!
           title: String!
@@ -37,24 +48,48 @@ app.use(
             mutation: RootMutation
         }
     `),
-		rootValue: {
-			events: () => {
-				return events;
-			},
-			createEvent: args => {
-				const event = {
-					_id: Math.random().toString(),
-					title: args.eventInput.title,
-					description: args.eventInput.description,
-					price: +args.eventInput.price,
-					date: args.eventInput.date
-				};
-				events.push(event);
-				return event;
-			}
-		},
-		graphiql: true
-	})
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch((err) => {
+            throw err;
+          });
+      },
+      createEvent: (args) => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+        });
+        return event
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
+      },
+    },
+    graphiql: true,
+  })
 );
 
-app.listen(3000);
+//access env vars
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `\nThe server is running on port ${PORT}`
+      .brightBlue.bold
+  )
+);
