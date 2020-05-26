a7c4b9ab3981bb7867557e43f2e42e2c09c69f8b
jashmehta3300 <jashmehta3300@gmail.com>
2020-05-26 17:01:03 +0530

    navbar added

---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function MainNavigation() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Event-Manager
+          </Typography>
+          <Link to="/events" style={{ textDecoration: 'none', color: 'white' }}>
+            <Button color="inherit">Events</Button>
+          </Link>
+          <Link to="/bookings" style={{ textDecoration: 'none', color: 'white' }}>
+            <Button color="inherit">Bookings</Button>
+          </Link>
+          <Link to="/auth" style={{ textDecoration: 'none', color: 'white' }}>
+            <Button color="inherit">Login</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
